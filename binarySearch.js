aee43e2a39aca9d9d22a6290ac92ec800f093231
Benjamin <themonsterhidingunderyourbed@gmail.com>
2023-04-21 09:16:30 -0700

    added daily algos

---
+++ binarySearch.js
@@ -0,0 +1,62 @@
+/* 
+    Array: Binary Search (non recursive)
+    Given a sorted array and a value, return whether the array contains that value.
+    Do not sequentially iterate the array. Instead, 'divide and conquer',
+    taking advantage of the fact that the array is sorted .
+    Bonus (alumni interview): 
+    first complete it without the bonus, because they ask for additions
+    after the initial algo is complete
+    return how many times the given number occurs
+*/
+
+const nums1 = [1, 3, 5, 6];
+const searchNum1 = 4;
+const expected1 = false;
+
+const nums2 = [4, 5, 6, 8, 12];
+const searchNum2 = 5;
+const expected2 = true;
+
+const nums3 = [3, 4, 6, 8, 12];
+const searchNum3 = 3;
+const expected3 = true;
+
+// bonus, how many times does the search num appear?
+const nums4 = [2, 2, 2, 2, 3, 4, 5, 6, 7, 8, 9];
+const searchNum4 = 2;
+const expected4 = 4;
+
+function searchHalf(sortedNums, searchNum) {
+  if (searchNum == sortedNums[Math.floor(sortedNums.length/2)]) {
+    return true
+  }
+  else if (sortedNums.length == 1) {
+    return false
+  }
+  else {
+    binarySearch(sortedNums, searchNum)
+  }
+}
+/**
+ * Efficiently determines if the given num exists in the given array.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} sortedNums
+ * @param {number} searchNum
+ * @returns {boolean} Whether the given num exists in the given array.
+ */
+function binarySearch(sortedNums, searchNum) {
+  if (searchNum == sortedNums[Math.floor(sortedNums.length/2)]) {
+    return true
+  }
+  if (searchNum < sortedNums[Math.floor(sortedNums.length/2)]) {
+    sortedNums.splice(sortedNums.length/2, sortedNums.length/2)
+    searchHalf(sortedNums, searchNum)
+  }
+  if (searchNum > sortedNums[Math.floor(sortedNums.length/2)]) {
+    sortedNums.splice(0, sortedNums.length/2)
+    searchHalf(sortedNums, searchNum)
+  }
+  return searchHalf(sortedNums, searchNum)
+}
+console.log(binarySearch(nums1, searchNum1));

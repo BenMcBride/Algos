bf9606d6c3f80be4cb7f4b08f0d796f3b69c0aa5
Benjamin <bjmcbride2@gmail.com>
2023-06-30 08:27:48 -0700

    daily algos

--- Data_Structures/DoublyLinkedList.js
+++ Data_Structures/DoublyLinkedList.js
@@ -27,6 +27,56 @@ class DoublyLinkedList {
   }
 
   /**
+   * Retrieves the data from the nthLast node in this list.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {number} nthLast Indicates the position from the back of the list.
+   * @returns {any}
+   */
+  nthToLast(nthLast) {
+    if (nthLast < 1 || this.isEmpty()) {
+      return null;
+    }
+    let slow = this.head;
+    let fast = this.head;
+    // Move the fast pointer nthLast positions ahead
+    for (let i = 0; i < nthLast; i++) {
+      if (fast === null) {
+        return null; // nthLast is out of range
+      }
+      fast = fast.next;
+    }
+    // Move both pointers together until the fast pointer reaches the end
+    while (fast !== null) {
+      slow = slow.next;
+      fast = fast.next;
+    }
+    return slow.data;
+  }
+
+  /**
+   * Determines if the node's data of this list forms a palindrome.
+   * - Time: O(n) where n is the length of the list.
+   * - Space: O(1).
+   * @returns {boolean} Indicates if this list is a palindrome.
+   */
+  isPalindrome() {
+    if (this.isEmpty()) {
+      return false;
+    }
+    let start = this.head;
+    let end = this.tail;
+    while (start !== end && start.prev !== end) {
+      if (start.data !== end.data) {
+        return false;
+      }
+      start = start.next;
+      end = end.prev;
+    }
+    return true;
+  }
+
+  /**
    * Creates a new node and adds it at the front of this list.
    * - Time: O(?).
    * - Space: O(?).
@@ -147,20 +197,27 @@ const emptyList = new DoublyLinkedList();
 const singleNodeList = new DoublyLinkedList().insertAtBack(1);
 const biNodeList = new DoublyLinkedList().insertAtBack(1).insertAtBack(2);
 const firstThreeList = new DoublyLinkedList().insertAtBackMany([1, 2, 3]);
+const palindromeList = new DoublyLinkedList().insertAtBackMany([1, 2, 3, 2, 1]);
 const secondThreeList = new DoublyLinkedList().insertAtBackMany([4, 5, 6]);
 const unorderedList = new DoublyLinkedList().insertAtBackMany([
   -5, -10, 4, -3, 6, 1, -7, -2,
 ]);
+// console.log("---");
+// console.log(secondThreeList.insertAtFront(9).toArray());
+// console.log("---");
+// console.log(unorderedList.toArray());
+// console.log(unorderedList.removeMiddleNode());
+// console.log(unorderedList.toArray());
+// console.log(unorderedList.removeMiddleNode());
+// console.log(unorderedList.toArray());
+// console.log("---");
+// console.log(singleNodeList.removeMiddleNode());
+// console.log(singleNodeList);
+// console.log(biNodeList.removeMiddleNode());
+// console.log(biNodeList.toArray());
 console.log("---");
-console.log(secondThreeList.insertAtFront(9).toArray());
+console.log(unorderedList.isPalindrome());
+console.log(palindromeList.isPalindrome());
 console.log("---");
-console.log(unorderedList.toArray());
-console.log(unorderedList.removeMiddleNode());
-console.log(unorderedList.toArray());
-console.log(unorderedList.removeMiddleNode());
-console.log(unorderedList.toArray());
-console.log("---");
-console.log(singleNodeList.removeMiddleNode());
-console.log(singleNodeList);
-console.log(biNodeList.removeMiddleNode());
-console.log(biNodeList.toArray());
+console.log(unorderedList.nthToLast(2));
+console.log(palindromeList.nthToLast(2));
